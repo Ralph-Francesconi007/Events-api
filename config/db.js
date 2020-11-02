fc7cbea554f66ef1433ee7e2851b14c1c51ece97
ralph.francesconi007 <ralph.francesconi007@gmail.com>
2020-11-02 16:17:49 -0500

    made some changes to the db.js file

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // creating a base name for the mongodb
-const mongooseBaseName = 'https://warm-shelf-34645.herokuapp.com/'
+const mongooseBaseName = 'express-api-template'
 
 // create the mongodb uri for development and test
 const database = {
@@ -15,6 +15,6 @@ const localDb = process.env.TESTENV ? database.test : database.development
 
 // Environment variable MONGODB_URI will be available in
 // heroku production evironment otherwise use test or development db
-const currentDb = process.env.MONGODB_URI || localDb
+const currentDb = process.env.DB_URI || localDb
 
 module.exports = currentDb
